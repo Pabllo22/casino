40e76d4d0c774ff8ac5394d84a39085ba6fbc1c2
Pavel Zuev <pm.zuev@dats.team>
2025-10-25 19:15:33 +0300

    fixed build

--- src/pages/casino/ui/CardInfo.tsx
+++ src/pages/casino/ui/CardInfo.tsx
@@ -1,7 +1,7 @@
 import { ChipsGroup, CustomSelect } from '@/shared/ui';
 import { Input } from '@/shared/ui/input/Input';
 import { useState } from 'react';
-import imageUrl from "@/shared/assets/wheel.png?url";
+import imageUrl from "@/shared/assets/Wheel.png";
 import { useFilters } from '@/shared/hooks';
 
 export const CardInfo = ({ onPromocodeChange }: { onPromocodeChange: (promocode: string) => void }) => {
